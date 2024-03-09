85348c4269e68279b9dca659fea2062d9a045dee
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2024-03-09 13:52:03 +0100

    create Admin Controllers and Home Controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Setting;
+use App\Models\Speaker;
+use App\Models\Schedule;
+use App\Models\Venue;
+use App\Models\Hotel;
+use App\Models\Gallery;
+use App\Models\Sponsor;
+use App\Models\Faq;
+use App\Models\Amenity;
+use App\Models\Price;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $settings = Setting::pluck('value', 'key');
+        $speakers = Speaker::all();
+        $schedules = Schedule::with('speaker')
+            ->orderBy('start_time', 'asc')
+            ->get()
+            ->groupBy('day_number');
+        $venues = Venue::all();
+        $hotels = Hotel::all();
+        $galleries = Gallery::all();
+        $sponsors = Sponsor::all();
+        $faqs = Faq::all();
+        $prices = Price::with('amenities')->get();
+        $amenities = Amenity::with('prices')->get();
+
+        return view('home', compact('settings', 'speakers', 'schedules', 'venues', 'hotels', 'galleries', 'sponsors', 'faqs', 'prices', 'amenities'));
+    }
+
+    public function view(Speaker $speaker)
+    {
+        $settings = Setting::pluck('value', 'key');
+        
+        return view('speaker', compact('settings', 'speaker'));
+    }
+}
